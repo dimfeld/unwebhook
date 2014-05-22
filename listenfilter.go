16ae6acc0d0c5efbd36e287f4d6f1afbce4334a5
Daniel Imfeld <daniel@danielimfeld.com>
2014-05-22 02:15:20 -0500

    Make ListenFilter work with IP prefixes

--- listenfilter.go
+++ listenfilter.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"github.com/dimfeld/glog"
 	"net"
+	"strings"
 )
 
 const (
@@ -14,7 +16,26 @@ type ListenFilter struct {
 	net.Listener
 	// BlackList or WhiteList.
 	Behavior   int
-	FilterAddr map[string]bool
+	FilterNet  []*net.IPNet
+	FilterAddr []net.IP
+}
+
+func (f *ListenFilter) AddString(s string) error {
+	if strings.Contains(s, "/") {
+		_, net, err := net.ParseCIDR(s)
+		if err != nil {
+			return err
+		}
+		f.FilterNet = append(f.FilterNet, net)
+	} else {
+		addr := net.ParseIP(s)
+		if addr == nil {
+			return errors.New("Invalid address")
+		}
+		f.FilterAddr = append(f.FilterAddr, addr)
+	}
+
+	return nil
 }
 
 func (f *ListenFilter) Accept() (c net.Conn, err error) {
@@ -24,19 +45,40 @@ func (f *ListenFilter) Accept() (c net.Conn, err error) {
 			return
 		}
 
-		addr := c.RemoteAddr().String()
-		addr, _, err = net.SplitHostPort(addr)
-		configured := f.FilterAddr[addr]
+		var addrStr string
+		addrStr, _, err = net.SplitHostPort(c.RemoteAddr().String())
+		addr := net.ParseIP(addrStr)
 
-		if (configured && f.Behavior == WhiteList) ||
-			(!configured && f.Behavior == BlackList) {
+		// A trie would be better here. But for this program there will rarely
+		// be more than one or two entries so it doesn't really matter.
 
+		found := false
+		for _, net := range f.FilterNet {
+			if net.Contains(addr) {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			for _, filterAddr := range f.FilterAddr {
+				if filterAddr.Equal(addr) {
+					found = true
+					break
+				}
+			}
+		}
+
+		if (found && f.Behavior == WhiteList) ||
+			(!found && f.Behavior == BlackList) {
+
+			// Connection allowed.
 			return
 		}
 
+		// Connection denied. Just close it silently.
 		c.Close()
-
-		glog.Infoln("Denied connection from", addr)
+		glog.Warningln("Denied connection from", addr)
 	}
 }
 
@@ -44,6 +86,7 @@ func NewListenFilter(l net.Listener, behavior int) *ListenFilter {
 	return &ListenFilter{
 		Listener:   l,
 		Behavior:   behavior,
-		FilterAddr: make(map[string]bool),
+		FilterAddr: make([]net.IP, 0),
+		FilterNet:  make([]*net.IPNet, 0),
 	}
 }
